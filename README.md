bad5194878bcceb3aa5408c106d97229a3041d55
Laureat-commit <freefireneutre23@gmail.com>
2025-05-25 14:00:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# STORM-RP-BY-WASUKE-
-Welcome to you, I hope this bot could help you in your multiple tasks. 🧸
+# KAMDAO UCHIWA 
+BOT ANIMÉ POUR LE RP
+
+# PREND TON CHAKRA 
+<a href='https://raizenbot-webpair.onrender.com' target="_blank"><img alt='GET SESSION' src='https://img.shields.io/badge/Click here to get your Session code-blue?style=for-the-badge&logo=opencv&logoColor=white'/></a> 
+<a><img src='https://files.catbox.moe/hwdb5h.png'/></a>    
+
---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "4ZYygCZD#jcJhe6SZCURnf3I1nWqFOuFeZQy9eXds7GBKYbltKeo", 
+MONGODB: process.env.MONGODB || "mongodb+srv://raizen:darki123@cluster0.utsln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
+};
